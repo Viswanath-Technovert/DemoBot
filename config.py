f6d8f8d94c23b3438b8c83bc0e0498ef960d24d2
Viswanath Chaluvadi <viswanath.c@Technovert.com>
2024-01-30 17:17:33 +0530

    app id and password

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "b684497f-2ff5-4b6b-a098-43e67a30d53e")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "j558Q~qKOViS.DgZNL8XxX9CjbC.yYW5iFfN3cM2")
+    APP_ID = os.environ.get("MicrosoftAppId", "8edc6723-1c38-4049-ac15-3c062185c092")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "cUY8Q~qk~cRySscMSXdcI8OCy56i8tAWun_mBat~")
     # APP_ID = os.environ.get("MicrosoftAppId", "")
     # APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
